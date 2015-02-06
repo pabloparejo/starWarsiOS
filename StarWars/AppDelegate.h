c9796842b6092f15e44b92f0cb7228e687b6b49a
Pablo Parejo <parejo@Pablos-MacBook-Pro.local>
2015-02-06 18:09:47 +0100

    Initial Commit

---
+++ StarWars/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StarWars
+//
+//  Created by Pablo Parejo Camacho on 6/2/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
