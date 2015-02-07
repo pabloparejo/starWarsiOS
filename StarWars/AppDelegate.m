804185a60f90a09b9c3f85ccb1b8e385873b696f
Pablo Parejo <parejo@Pablos-MacBook-Pro.local>
2015-02-07 12:47:51 +0100

    Adding more models and controllers to TabBarController

--- StarWars/AppDelegate.m
+++ StarWars/AppDelegate.m
@@ -20,20 +20,12 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
-    // Creo el modelo
-    NSURL *vaderURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/r2d2"];
-    NSBundle *bundle = [NSBundle mainBundle];
-    NSData *vaderSound = [NSData dataWithContentsOfURL:[bundle URLForResource:@"r2-d2"withExtension:@"caf"]];
-    UIImage *vaderImage = [UIImage imageNamed:@"R2-D2.jpg"];
-    PARStarWarsCharacter *c3po = [PARStarWarsCharacter starWarsCharacterWithAlias:@"R2D2"                                                                           url:vaderURL                                                                           sound:vaderSound                                                                           image:vaderImage];
-    // Creo el controlador
-    PARCharacterViewController *c3poVC = [[PARCharacterViewController alloc] initWithModel:c3po];
     
-    // Creo el combinador
-    UINavigationController *navVC = [[UINavigationController alloc] initWithRootViewController:c3poVC];
+    UITabBarController *tabVC = [[UITabBarController alloc] init];
+    [tabVC setViewControllers:[self arrayOfControllers] animated:YES];
     
     // Muestro vista en pantalla
-    [self.window setRootViewController:navVC];
+    [self.window setRootViewController:tabVC];
     
     return YES;
 }
@@ -59,5 +51,96 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+#pragma mark - Utils
+-(NSArray*) arrayOfModels{
+    // Creo el modelo r2d2
+    NSURL *r2d2URL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/r2d2"];
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSData *r2d2Sound = [NSData dataWithContentsOfURL:[bundle URLForResource:@"r2-d2"withExtension:@"caf"]];
+    UIImage *r2d2Image = [UIImage imageNamed:@"R2-D2.jpg"];
+    PARStarWarsCharacter *r2d2 = [PARStarWarsCharacter
+                                  starWarsCharacterWithAlias:@"R2D2"
+                                  url:r2d2URL
+                                  sound:r2d2Sound
+                                  image:r2d2Image];
+    
+    
+    // Creo el modelo chewbaca
+    NSURL *chewiURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/chewbacca"];
+    NSData *chewiSound = [NSData dataWithContentsOfURL:[bundle URLForResource:@"chewbacca"withExtension:@"caf"]];
+    UIImage *chewiImage = [UIImage imageNamed:@"chewbacca.jpg"];
+    PARStarWarsCharacter *chewi = [PARStarWarsCharacter
+                                   starWarsCharacterWithAlias:@"chewbacca"
+                                   url:chewiURL
+                                   sound:chewiSound
+                                   image:chewiImage];
+    
+    // Creo el modelo c3po
+    NSURL *c3poURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/c3po"];
+    NSData *c3poSound = [NSData dataWithContentsOfURL:[bundle URLForResource:@"c3po"withExtension:@"caf"]];
+    UIImage *c3poImage = [UIImage imageNamed:@"c3po.jpg"];
+    PARStarWarsCharacter *c3po = [PARStarWarsCharacter
+                                  starWarsCharacterWithAlias:@"C3PO"
+                                  url:c3poURL
+                                  sound:c3poSound
+                                  image:c3poImage];
+
+
+    // Creo el modelo vader
+    NSURL *vaderURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/darth_vader"];
+        NSData *vaderSound = [NSData dataWithContentsOfURL:[bundle URLForResource:@"vader"withExtension:@"caf"]];
+    UIImage *vaderImage = [UIImage imageNamed:@"darthVader.jpg"];
+    PARStarWarsCharacter *vader = [PARStarWarsCharacter
+                                  starWarsCharacterWithName: @"Anakin Skywalker"
+                                  alias:@"Vader"
+                                  url:vaderURL
+                                  sound:vaderSound
+                                  image:vaderImage];
+    
+    // Modelo Tarkin
+    NSURL *tarkinURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/tarkin"];
+    NSData *tarkinSound = [NSData dataWithContentsOfURL:[bundle URLForResource:@"tarkin"withExtension:@"caf"]];
+    UIImage *tarkinImage = [UIImage imageNamed:@"tarkin.jpg"];
+    PARStarWarsCharacter *tarkin = [PARStarWarsCharacter
+                                   starWarsCharacterWithName: @"Tarkin"
+                                   alias:@"Grand Moff Tarkin"
+                                   url:tarkinURL
+                                   sound:tarkinSound
+                                   image:tarkinImage];
+
+    
+    // Modelo palpatine
+    NSURL *palpatineURL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/palpatine"];
+    NSData *palpatineSound = [NSData dataWithContentsOfURL:[bundle URLForResource:@"palpatine"withExtension:@"caf"]];
+    UIImage *palpatineImage = [UIImage imageNamed:@"palpatine.jpg"];
+    PARStarWarsCharacter *palpatine = [PARStarWarsCharacter
+                                    starWarsCharacterWithName: @"palpatine"
+                                    alias:@"Darth Sidious"
+                                    url:palpatineURL
+                                    sound:palpatineSound
+                                    image:palpatineImage];
+    
+    return @[vader, chewi, r2d2, c3po, tarkin, palpatine];
+}
+
+-(NSArray *) arrayOfControllers{
+    
+    NSArray *models = [self arrayOfModels];
+    NSMutableArray *controllers = [NSMutableArray arrayWithCapacity:models.count];
+    
+    for (PARStarWarsCharacter *model in models) {
+        PARCharacterViewController *modelVC = [[PARCharacterViewController alloc] initWithModel:model];
+        
+        UINavigationController *navVC = [[UINavigationController alloc] initWithRootViewController:modelVC];
+        
+        [controllers addObject:navVC];
+    }
+    
+    return controllers;
+}
+
+
+
 
 @end
