3065fa61d539b7d5892d58d12686369fd8bbdce5
Pablo Parejo <parejo@Pablos-MacBook-Pro.local>
2015-03-25 20:51:03 +0100

    adding custom cell clases and assets

---
+++ StarWars/PARStarWarsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PARStarWarsTableViewCell.h
+//  StarWars
+//
+//  Created by Pablo Parejo Camacho on 28/2/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PARStarWarsTableViewCell : UITableViewCell
+
+@end
---
+++ StarWars/PARStarWarsTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PARStarWarsTableViewCell.m
+//  StarWars
+//
+//  Created by Pablo Parejo Camacho on 28/2/15.
+//  Copyright (c) 2015 Pablo Parejo Camacho. All rights reserved.
+//
+
+#import "PARStarWarsTableViewCell.h"
+
+@implementation PARStarWarsTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
